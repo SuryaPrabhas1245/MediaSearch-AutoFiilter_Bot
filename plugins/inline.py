717630b047bd5b7a26e170860b8468e56aa9a05a
SuryaPrabhas1245 <85777749+SuryaPrabhas1245@users.noreply.github.com>
2021-11-28 11:34:24 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,8 +90,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('📡sʜᴀʀᴇ📡', url='https://t.me/share/url?url=%20https://t.me/TeluguDubbedHorrorMovies2'),
-            InlineKeyboardButton('Our-LinkZ', url='https://t.me/TeluguDubbedHorrorMovies2')
+            InlineKeyboardButton('📡Share Bot📡', url='https://t.me/share/url?url=%20https://t.me/TeluguDubbedHorrorMovies2'),
+            InlineKeyboardButton('Share Channel', url='https://t.me/TeluguDubbedHorrorMovies2')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
